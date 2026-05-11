8bf7f19758b363dc6e0628b82308640363b32216
Dulneth Santhuka <dulneth.sa@gmail.com>
2026-05-11 22:33:22 +0530

    Refactor lesson 01 variables: comment out initial variable definitions and print statements

--- phase_01_fundamentals/lesson/lesson_01_variables.py
+++ phase_01_fundamentals/lesson/lesson_01_variables.py
@@ -1,18 +1,36 @@
 # lesson 01: variable in python
 
-student_name = "John Doe"
-student_age = 20
-student_course = "Software Engineering"
-student_year = 4
+# student_name = "John Doe"
+# student_age = 20
+# student_course = "Software Engineering"
+# student_year = 4
+# is_student = True
+# has_completed_degree = False
 
-print("Student Name:", student_name)
-print("Student Age:", student_age)
-print("Student Course:", student_course)
-print("Student Year:", student_year)
+# print("Student Name:", student_name)
+# print("Student Age:", student_age)
+# print("Student Course:", student_course)
+# print("Student Year:", student_year)
+# print("Is the student currently enrolled?", is_student)
+# print("Has the student completed their degree?", has_completed_degree)
 
-# check variable type
-print("Type of the student name variable:", type(student_name))
-print("Type of the student age variable:", type(student_age))
-print("Type of the student course variable:", type(student_course))
-print("Type of the student year variable:", type(student_year))
+# # check variable type
+# print("Type of the student name variable:", type(student_name))
+# print("Type of the student age variable:", type(student_age))
+# print("Type of the student course variable:", type(student_course))
+# print("Type of the student year variable:", type(student_year))
+# print("Type of the is_student variable:", type(is_student))
+# print("Type of the has_completed_degree variable:", type(has_completed_degree))
 
+
+#==============Type conversion================
+# age_text = "20"
+# age_number = int(age_text)
+# print("Age (as a number):", age_number)
+# print("Type of the age_number variable:", type(age_number))
+
+age = 22
+
+age_txt = str(age)
+print("Age (as text):", age_txt)
+print("Type of the age_txt", type(age_txt))
