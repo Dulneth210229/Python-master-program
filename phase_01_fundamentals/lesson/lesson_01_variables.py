2fea0f7c578c35fbb12c39166a6e06967f00c626
Dulneth Santhuka <dulneth.sa@gmail.com>
2026-05-11 21:57:01 +0530

    Add initial implementation of variable demonstration in Python

---
+++ phase_01_fundamentals/lesson/lesson_01_variables.py
@@ -0,0 +1,11 @@
+#lesson 01: variable in python
+
+student_name = "John Doe"
+student_age = 20
+student_course = "Software Engineering"
+student_year = 4
+
+print("Student Name:", student_name)
+print("Student Age:", student_age)
+print("Student Course:", student_course)
+print("Student Year:", student_year)
